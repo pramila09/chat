1a1fe60b10ccddd9a1d210efa27857fba26ca5fb
pramila09 <pramila.shawol@gmail.com>
2020-05-22 01:04:39 +0545

    First Commit

---
+++ resources/js/app.js
@@ -0,0 +1,134 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+require('./bootstrap');
+
+window.Vue = require('vue');
+//for auto scroll
+import Vue from 'vue'
+
+import VueChatScroll from 'vue-chat-scroll'
+Vue.use(VueChatScroll)
+
+//for notification
+import Toaster from 'v-toaster'
+import 'v-toaster/dist/v-toaster.css'
+Vue.use(Toaster, {timeout: 5000})
+
+
+// const files = require.context('./', true, /\.vue$/i)
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+
+Vue.component('message', require('./components/message.vue').default);
+
+const app = new Vue({
+    el: '#app',
+    data:{
+        message:'',
+        chat:{
+            message:[],
+            user:[],
+            color:[],
+            time:[],
+        },
+        typing:'',
+        numberOfUsers:0,
+    },
+    watch:{
+        message(){
+            Echo.private('chat')
+                .whisper('typing', {
+                    name: this.message
+                });
+        }
+    },
+    methods:{
+        send(){
+            if(this.message.length !==0)
+            {
+                this.chat.message.push(this.message);
+                this.chat.color.push('success');
+                this.chat.user.push('You');
+                this.chat.time.push(this.getTime());
+                axios.post('/send', {
+                    message:this.message,
+                    chat:this.chat,
+                })
+                    .then(response => {
+                        console.log(response);
+                        this.message='';
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    });
+            }
+
+        },
+        getTime(){
+            let time=new Date();
+            return time.getHours()+':'+time.getMinutes();
+        },
+        getOldMessages(){
+            axios.post('/getOldMessage')
+                .then(response=>{
+                    console.log(response);
+                    if(response.data !==''){
+                        this.chat=response.data;
+                    }
+
+                })
+                .catch(error=>{
+                    console.log(error);
+                });
+        },
+        deleteSession(){
+            axios.post('/deleteSession')
+            .then(response=>this.$toaster.success('Chat history is deleted'));
+        }
+    },
+    mounted(){
+        this.getOldMessages();
+        Echo.private('chat')
+            .listen('ChatEvent', (e) => {
+                this.chat.message.push(e.message);
+                this.chat.color.push('warning');
+                this.chat.user.push(e.user);
+                this.chat.time.push(this.getTime());
+                axios.post('/saveToSession',{
+                    chat: this.chat
+                })
+                    .then(response=>{
+
+                    })
+                    .catch(error=>{
+                        console.log(error);
+                    });
+                //console.log(e);
+            })
+            .listenForWhisper('typing', (e) => {
+                if(e.name !== ''){
+                    this.typing='typing...';
+                }else{
+                    this.typing='';
+                }
+
+            });
+
+        Echo.join(`chat`)
+            .here((users) => {
+                this.numberOfUsers=users.length;
+            })
+            .joining((user) => {
+                this.numberOfUsers +=1;
+                //console.log(user);
+                this.$toaster.success(user.name+ ' has joined the chat room');
+            })
+            .leaving((user) => {
+                this.numberOfUsers -= 1;
+                this.$toaster.warning(user.name+ ' has leaved the chat room');
+            });
+    }
+});
---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('chat','ChatController@chat');
+Route::post('send','ChatController@send');
+Route::post('getOldMessage','ChatController@getOldMessage');
+Route::post('saveToSession','ChatController@saveToSession');
+Route::post('deleteSession','ChatController@deleteSession');
+
+Route::get('check',function (){
+    return session('chat');
+});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
